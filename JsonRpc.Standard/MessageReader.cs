f661c1cd3e143752a50f16120ddbf5c5a8ced51c
forest93 <forest93@163.com>
2017-04-21 19:35:34 +0800

    Add documentation.

--- JsonRpc.Standard/MessageReader.cs
+++ JsonRpc.Standard/MessageReader.cs
@@ -1,7 +1,17 @@
 ﻿namespace JsonRpc.Standard
 {
+    /// <summary>
+    /// Represents a JSON RPC message reader.
+    /// </summary>
     public abstract class MessageReader
     {
+        /// <summary>
+        /// Reads the next message.
+        /// </summary>
+        /// <returns>
+        /// The next JSON RPC message, or <c>null</c> if no more messages exist.
+        /// </returns>
+        /// <remarks>This method might block the caller thread until the next message is available.</remarks>
         public abstract Message Read();
     }
 }
--- JsonRpc.Standard/MessageWriter.cs
+++ JsonRpc.Standard/MessageWriter.cs
@@ -1,7 +1,17 @@
-﻿namespace JsonRpc.Standard
+﻿using System;
+
+namespace JsonRpc.Standard
 {
+    /// <summary>
+    /// Represents a JSON RPC message writer.
+    /// </summary>
     public abstract class MessageWriter
     {
+        /// <summary>
+        /// Writes a message.
+        /// </summary>
+        /// <param name="message">The meesage to write.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="message"/> is <c>null</c>.</exception>
         public abstract void Write(Message message);
     }
 }
