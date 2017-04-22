5fcad3422e5cf8d7e90e133a25a01e7721b25e1a
forest93 <forest93@163.com>
2017-04-23 02:25:43 +0800

    Add a simple test case.

--- UnitTestProject1/ServerTest.cs
+++ UnitTestProject1/ServerTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using JsonRpc.Standard;
+using JsonRpc.Standard.Server;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -9,8 +13,15 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            var request = "{\"jsonrpc\": \"2.0\",\"id\": 1,\"method\": \"sum\",\"params\": {\"a\":100, \"b\":-200}}";
-
+            var request = "{\"jsonrpc\": \"2.0\",\"id\": 1,\"method\": \"sum\",\"params\": {\"x\":100, \"y\":-200}}";
+            using (var reader = new StringReader(request))
+            using (var writer = new StringWriter())
+            {
+                var host = Utility.CreateJsonRpcServiceHost(new ByLineTextMessageReader(reader),
+                    new ByLineTextMessageWriter(writer));
+                host.RunAsync().Wait();
+                Trace.WriteLine(writer.ToString());
+            }
         }
     }
 }
