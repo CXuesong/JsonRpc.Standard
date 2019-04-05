11d64aff4b53791664fb2a400c941af74fbc4900
forest93 <forest93@163.com>
2019-04-05 22:28:44 +0800

    Introduce JsonRpc.WebSockets! You need to use them (almost) in the same way as other Stream-based message readers/writers.

---
+++ JsonRpc.WebSockets/WebSocketMessageReader.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using JsonRpc.Standard;
+using JsonRpc.Standard.Server;
+using JsonRpc.Streams;
+
+namespace JsonRpc.WebSockets
+{
+    /// <summary>
+    /// A <see cref="MessageReader"/> implementation that reads JSON-RPC message from <see cref="WebSocket"/>.
+    /// </summary>
+    /// <remarks>
+    /// You can use this class with <see cref="StreamRpcServerHandler"/> or <see cref="StreamRpcClientHandler"/>.
+    /// </remarks>
+    public class WebSocketMessageReader : QueuedMessageReader
+    {
+
+        private readonly int bufferSize;
+        private readonly int maxMessageLength;
+        private readonly TaskCompletionSource<WebSocketCloseStatus> webSocketCloseTcs;
+
+        /// <inheritdoc cref="WebSocketMessageReader(WebSocket, int)"/>
+        /// <remarks>This implementation allows each message to be as large as 4GB.</remarks>
+        public WebSocketMessageReader(WebSocket webSocket) : this(webSocket, 4096, int.MaxValue)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a message reader from <see cref="WebSocket"/>.
+        /// </summary>
+        /// <param name="webSocket">The WebSocket to read messages from.</param>
+        /// <param name="bufferSize">Length of the internal buffer kept by the message reader.</param>
+        /// <param name="maxMessageLength">The maximum allowed reading message length (bytes) per JSON-RPC message.</param>
+        public WebSocketMessageReader(WebSocket webSocket, int bufferSize, int maxMessageLength)
+        {
+            if (bufferSize <= 0) throw new ArgumentOutOfRangeException(nameof(bufferSize));
+            if (maxMessageLength <= 0) throw new ArgumentOutOfRangeException(nameof(maxMessageLength));
+            WebSocket = webSocket ?? throw new ArgumentNullException(nameof(webSocket));
+            this.bufferSize = bufferSize;
+            this.maxMessageLength = maxMessageLength;
+            webSocketCloseTcs = new TaskCompletionSource<WebSocketCloseStatus>();
+        }
+
+        /// <summary>
+        /// Gets the underlying <see cref="WebSocket"/>.
+        /// </summary>
+        public WebSocket WebSocket { get; }
+
+        /// <summary>
+        /// Gets a Task that completes with status code when the underlying WebSocket is closed.
+        /// </summary>
+        public Task<WebSocketCloseStatus> WebSocketClose => webSocketCloseTcs.Task;
+
+        /// <inheritdoc />
+        protected override async Task<Message> ReadDirectAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var ms = new MemoryStream())
+            {
+                var buffer = new byte[bufferSize];
+                while (true)
+                {
+                    var result = await WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+                    if (result.CloseStatus != null)
+                        webSocketCloseTcs.TrySetResult(result.CloseStatus.Value);
+                    if (result.Count > 0)
+                    {
+                        if (ms.Length + result.Count > maxMessageLength)
+                            // perhaps we need to close the channel now.
+                            throw new InvalidOperationException("Received message is too long.");
+                        ms.Write(buffer, 0, result.Count);
+                    }
+                    if (result.EndOfMessage)
+                    {
+                        if (ms.Length > 0)      // If the socket is closing, we won't receive any data in the reception result.
+                        {
+                            ms.Seek(0, SeekOrigin.Begin);
+                            // Use UTF-8 by default.
+                            using (var reader = new StreamReader(ms))
+                            {
+                                return Message.LoadJson(reader);
+                            }
+                        }
+                    }
+                    if (result.CloseStatus != null)
+                    {
+                        // Message not ended, but the socket closed.
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ JsonRpc.WebSockets/WebSocketMessageWriter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using JsonRpc.Standard;
+using JsonRpc.Streams;
+
+namespace JsonRpc.WebSockets
+{
+    /// <summary>
+    /// A <see cref="MessageWriter"/> implementation that writes JSON-RPC message into <see cref="WebSocket"/>.
+    /// </summary>
+    /// <remarks>
+    /// You can use this class with <see cref="StreamRpcServerHandler"/> or <see cref="StreamRpcClientHandler"/>.
+    /// </remarks>
+    public class WebSocketMessageWriter : MessageWriter
+    {
+
+        private readonly int chunkSize;
+
+        /// <inheritdoc cref="WebSocketMessageWriter(WebSocket, int)"/>
+        public WebSocketMessageWriter(WebSocket webSocket) : this(webSocket, 4096)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a message reader from <see cref="WebSocket"/>.
+        /// </summary>
+        /// <param name="webSocket">The WebSocket to write messages into.</param>
+        /// <param name="chunkSize">The maximum size in bytes of each JSON-RPC frame when sending messages.</param>
+        public WebSocketMessageWriter(WebSocket webSocket, int chunkSize)
+        {
+            if (chunkSize <= 0) throw new ArgumentOutOfRangeException(nameof(chunkSize));
+            WebSocket = webSocket ?? throw new ArgumentNullException(nameof(webSocket));
+            this.chunkSize = chunkSize;
+        }
+
+        /// <summary>
+        /// Gets the underlying <see cref="WebSocket"/>.
+        /// </summary>
+        public WebSocket WebSocket { get; }
+
+        /// <inheritdoc />
+        public override async Task WriteAsync(Message message, CancellationToken cancellationToken)
+        {
+            using (var ms = new MemoryStream())
+            using (var writer = new StreamWriter(ms))
+            {
+                message.WriteJson(writer);
+                writer.Flush();
+                var pos = 0;
+                if (!ms.TryGetBuffer(out var buffer))
+                {
+                    Debug.Fail("We should get buffer successfully.");
+                    buffer = new ArraySegment<byte>(ms.ToArray());
+                }
+                while (pos < buffer.Count)
+                {
+                    var length = Math.Min(chunkSize, buffer.Count - pos);
+                    var isEom = pos + length >= buffer.Count;
+                    await WebSocket.SendAsync(new ArraySegment<byte>(buffer.Array, pos, length), WebSocketMessageType.Text, isEom, cancellationToken);
+                    pos += length;
+                }
+            }
+        }
+    }
+}
